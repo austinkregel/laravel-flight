bf3a9702fc5e7b38eb20c2e677fcc7d53e60842f
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2023-03-19 23:05:19 -0400

    Add config file

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'prefix' => env('FLIGHT_URL_PREFIX', '/flight'),
+    // The driver is the default authentication provider when visiting /login
+    'driver' => env('FLIGHT_DRIVER', 'github'),
+    'user_model' => App\Models\User::class,
+    // Disabling registration will not impact logging in. Those who have already logged in
+    // can still do so by visiting the login url.
+    'registration' => env('FLIGHT_ALLOW_REGISTRATION', true),
+    'post_login_redirect' => env('FLIGHT_LOGIN_REDIRECT', '/horizon'),
+    'community_drivers' => [
+        SocialiteProviders\LaravelPassport\LaravelPassportExtendSocialite::class,
+    ],
+    'stateless' => true,
+    'scopes' => ['email', 'profile'],
+    'middleware' => ['web', 'auth'],
+];
